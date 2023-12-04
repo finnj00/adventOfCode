1df3a47a06e1b8fd5801702344713f60857415e5
Finn J <soul_sap0t@icloud.com>
2023-12-04 04:50:45 -0800

    Completed star 2 and added short file for debugging

--- Day1/main.py
+++ Day1/main.py
@@ -1,10 +1,12 @@
 from solution import Solution
 
 INPUT_FILENAME = "./input.txt"
+SHORT = "./short.txt"
 
 def main():
     s = Solution()
-    print(s.trebuchet(INPUT_FILENAME))
+    print("Star 1: " + str(s.trebuchet(INPUT_FILENAME)))
+    print("Star 2: " + str(s.trebuchet2(INPUT_FILENAME)))
 
 if __name__ == "__main__":
     main()
--- Day1/solution.py
+++ Day1/solution.py
@@ -20,7 +20,6 @@ class Solution:
         for line in f:
             first = None
             last = None
-            cur = ''
             for char in line:
                 if char.isdigit():
                     if first is None:
@@ -32,3 +31,43 @@ class Solution:
             sum += int(combined)
         f.close()
         return sum
+    
+    def trebuchet2(self, filename):
+        f = open(filename, 'r')
+        sum = 0
+        for line in f:
+            line = self.preprocess(line)
+            first = None
+            last = None
+            for char in line:
+                if char.isdigit():
+                    if first is None:
+                        first = char
+                        last = char
+                    else:
+                        last = char            
+            combined = first + last
+            sum += int(combined)
+        f.close()
+        return sum
+    
+    def preprocess(self, line):
+        memo = [0] * len(line)
+        newLine = ''
+        for spelledNum in self.spelledNums.keys():
+            if len(line) < len(spelledNum):
+                pass
+            else:
+                start = 0
+                end = len(spelledNum)
+                while end <= len(line):
+                    if line[start:end] == spelledNum:
+                        memo[start] = self.spelledNums[spelledNum]
+                    start += 1
+                    end += 1
+        for i in range(len(line)):
+            if memo[i] != 0:
+                newLine += str(memo[i])
+            else:
+                newLine += line[i]  
+        return newLine
